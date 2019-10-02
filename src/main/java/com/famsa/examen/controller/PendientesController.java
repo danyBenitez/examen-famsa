5799b016208ed501d7d4b89c05e3326755e1e2d0
danyBenitez <47193079+danyBenitez@users.noreply.github.com>
2019-10-02 13:33:11 -0500

    test

--- src/main/java/com/famsa/examen/controller/PendientesController.java
+++ src/main/java/com/famsa/examen/controller/PendientesController.java
@@ -13,6 +13,7 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 import com.famsa.examen.model.Pendiente;
 import com.famsa.examen.service.PendienteService;
